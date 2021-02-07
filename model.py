e588d713ef7ff0592ce4f466e6fe0ae52e6680da
Luiz Gustavo Rocha <rocha.luizgustavo94@gmail.com>
2021-02-06 22:12:59 -0300

    corrigido erro de importação de biblioteca do pytorch

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 def hidden_init(layer):
     fan_in = layer.weight.data.size()[0]
@@ -34,8 +34,8 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        return F.tanh(self.fc2(x))
+        x = torch.relu(self.fc1(state))
+        return torch.tanh(self.fc2(x))
 
 
 class Critic(nn.Module):
@@ -67,8 +67,8 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.leaky_relu(self.fcs1(state))
+        xs = torch.leaky_relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = torch.leaky_relu(self.fc2(x))
+        x = torch.leaky_relu(self.fc3(x))
         return self.fc4(x)
